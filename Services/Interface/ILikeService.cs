f0d197f1cbf9b0b22056516a84c0096ecd41aaee
yagizerdem <yagizerdem819@gmail.com>
2024-10-28 09:27:01 +0300

    update repository and service

---
+++ Services/Interface/ILikeService.cs
@@ -0,0 +1,16 @@
+﻿using Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface ILikeService
+    {
+        Task PostLike(ArticleDTO articleDTO, string authorId);
+        Task<IEnumerable<ArticleDTO>> GetLikes(int page, int limit);
+        Task DeleteLike(int articleId, string authorId);
+    }
+}
---
+++ Services/ServiceClass/LikeService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Models.DTO;
+using Repository.UnitOfWork;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ServiceClass
+{
+    public class LikeService : ILikeService
+    {
+        private readonly IMapper _autoMapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public LikeService(IUnitOfWork unitOfWork, IMapper autoMapper)
+        {
+            _unitOfWork = unitOfWork;
+            this._autoMapper = autoMapper;
+        }
+        public Task DeleteLike(int articleId, string authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ArticleDTO>> GetLikes(int page, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostLike(ArticleDTO articleDTO, string authorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
